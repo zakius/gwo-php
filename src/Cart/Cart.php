4f521df8795d8b5f5e8fd48e3f96afc27b2720d1
zakius <jaroslaw.jarosik@outlook.com>
2019-05-22 17:44:21 +0200

    Clean after checkout

--- src/Cart/Cart.php
+++ src/Cart/Cart.php
@@ -5,12 +5,26 @@ namespace Recruitment\Cart;
 
 
 use OutOfBoundsException;
+use Recruitment\Entity\Order;
 use Recruitment\Entity\Product;
 
 class Cart
 {
     private $items;
 
+    /**
+     * Cart constructor.
+     */
+    public function __construct()
+    {
+        $this->items = [];
+    }
+
+    /**
+     * @param Product $product
+     *
+     * @return int
+     */
     private function getProductIndex(Product $product): int
     {
         $id = $product->getId();
@@ -24,12 +38,13 @@ class Cart
         return $index;
     }
 
-    public function __construct()
-    {
-        $this->items = [];
-    }
-
-    public function addProduct(Product $product, int $quantity): Cart
+    /**
+     * @param Product $product
+     * @param int     $quantity
+     *
+     * @return Cart
+     */
+    public function addProduct(Product $product, int $quantity = 1): Cart
     {
         $index = $this->getProductIndex($product);
         if ($index > -1) {
@@ -42,11 +57,9 @@ class Cart
         return $this;
     }
 
-    public function getItems()
-    {
-        return $this->items;
-    }
-
+    /**
+     * @return int
+     */
     public function getTotalPrice(): int
     {
         $totalPrice = 0;
@@ -56,6 +69,19 @@ class Cart
         return $totalPrice;
     }
 
+    /**
+     * @return array
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param int $index
+     *
+     * @return Item
+     */
     public function getItem(int $index): Item
     {
         if ($index < 0 || $index >= count($this->items)) {
@@ -64,14 +90,28 @@ class Cart
         return $this->items[$index];
     }
 
+    /**
+     * @param Product $product
+     *
+     * @return Cart
+     */
     public function removeProduct(Product $product): Cart
     {
         $index = $this->getProductIndex($product);
+        if ($index === -1) {
+            return $this;
+        }
         unset($this->items[$index]);
         $this->items = array_values($this->items);
         return $this;
     }
 
+    /**
+     * @param Product $product
+     * @param int     $quantity
+     *
+     * @return Cart
+     */
     public function setQuantity(Product $product, int $quantity): Cart
     {
         $index = $this->getProductIndex($product);
@@ -83,5 +123,16 @@ class Cart
         return $this;
     }
 
+    /**
+     * @param int $id
+     *
+     * @return Order
+     */
+    public function checkout(int $id)
+    {
+        $order = new Order($id, $this->items, $this->getTotalPrice());
+        $this->items = [];
+        return $order;
+    }
 
 }
